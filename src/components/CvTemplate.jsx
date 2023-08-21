3ff93d8a59918e643de99b1071f12a786ccd6e0e
Mvlprem <mvl.prem@outlook.com>
2023-08-21 12:57:11 +0530

    first commit

---
+++ src/components/CvTemplate.jsx
@@ -0,0 +1,11 @@
+import '../styles/CvTemplate.css'
+
+function CvTemplate() {
+  return (
+    <>
+      <p>Hello World</p>
+    </>
+  )
+}
+
+export default CvTemplate
---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import CvTemplate from './components/CvTemplate.jsx'
+import './main.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <CvTemplate />
+  </React.StrictMode>
+)
