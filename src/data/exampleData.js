8be03797e427fa41b040636f855d1255cf2ff899
Mvlprem <mvl.prem@outlook.com>
2023-08-26 16:44:42 +0530

    update example data

--- src/data/exampleData.js
+++ src/data/exampleData.js
@@ -11,18 +11,18 @@ const exampleData = {
   sections: {
     educations: [
       {
-        degree: 'Bachelors in Economics',
-        schoolName: 'London City University',
-        location: 'New York City, US',
-        startDate: '09/2021',
+        degree: 'Shadow Clone Jutsu',
+        schoolName: 'Konoha Ninja Academy',
+        location: 'Konohagakure, JP',
+        startDate: '10/2002',
         endDate: 'present',
         id: uuidv4(),
       },
       {
-        degree: "Master's Degree in Math",
-        schoolName: 'Hidden University',
-        location: 'New York City, US',
-        startDate: '08/2020',
+        degree: 'Wind Release: Rasenshuriken',
+        schoolName: 'Konoha Ninja Academy',
+        location: 'Konohagakure, JP',
+        startDate: '10/2002',
         endDate: 'present',
         id: uuidv4(),
       },
@@ -30,23 +30,23 @@ const exampleData = {
 
     experiences: [
       {
-        company: 'Umbrella Inc.',
-        title: 'UX & UI Designer',
-        place: 'New York City, US',
+        company: 'Mount Myōboku.',
+        title: 'Pain Assault',
+        place: 'Konohagakure, JP',
         description:
-          'Designed and prototyped user interface patterns for various clients in various industries, ranging from self-service apps within the telecommunications-sector to mobile games for IOS and Android',
-        start: '08/2020',
-        end: 'present',
+          'Using my newly learned senjutsu techniques and a perfected Wind Release: Rasenshuriken, I saved Tsunade and handily defeated the Asura, Human, Animal, and Naraka Paths, before the Deva Path regained enough strength to fight on its own again.',
+        start: '03/2010',
+        end: '04/2010',
         id: uuidv4(),
       },
       {
-        company: 'Black Mesa Labs',
-        title: 'UX Research Assistant',
-        place: 'Berlin, Germany',
+        company: 'Land of Ancestors',
+        title: 'Fourth Great Ninja War',
+        place: 'Kaguya icy dimension',
         description:
-          'Supported senior researchers on accessibility standards for the open web. Created and usability tested wireframes and prototypes. Produced interactive documentation for quick onboarding of new researchers.',
-        start: '04/2018',
-        end: '02/2019',
+          'Using the seal given to me by Hagoromo Ōtsutsuki, the Sage of Six Paths. The tailed beasts are removed from Kaguya body, she is made to spit out Madara, and she and Black Zetsu are trapped in the Six Paths — Chibaku Tensei that is, itself, stuck in another dimension.',
+        start: '05/2012',
+        end: '09/2016',
         id: uuidv4(),
       },
     ],
